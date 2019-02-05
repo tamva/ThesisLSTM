7ad6e2d1f75ecf5c914a94dbfcdb5ac9c4bc4d06
Panagiotis Tamvakidis <tamvakidis@outlook.com>
2019-02-05 21:53:09 +0200

    ThLSMT and HelpingFunctions uploaded
    The result is the average prediction of the city. There new numpy arrays created in order to find the average value

--- HelpingFunctions.py
+++ HelpingFunctions.py
@@ -49,31 +49,6 @@ def normalise_windows(window_data):
     return normalised_data
 
 
-# def predict_sequence_full(model, data, window_size):
-#     # Shift the window by 1 new prediction each time, re-run predictions on new window
-#     curr_frame = data[0]
-#     predicted = []
-#     for i in range(len(data)):
-#         predicted.append(model.predict(curr_frame[newaxis, :, :])[0, 0])
-#         curr_frame = curr_frame[1:]
-#         curr_frame = np.insert(curr_frame, [window_size - 1], predicted[-1], axis=0)
-#     return predicted
-
-
-# def predict_sequences_multiple(model, data, window_size, prediction_len):
-#     # Predict sequence of <prediction_len> steps before shifting prediction run forward by <prediction_len> steps
-#     prediction_seqs = []
-#     for i in range(int(len(data) / prediction_len)):
-#         curr_frame = data[i * prediction_len]
-#         predicted = []
-#         for j in range(prediction_len):
-#             predicted.append(model.predict(curr_frame[newaxis, :, :])[0, 0])
-#             curr_frame = curr_frame[1:]
-#             curr_frame = np.insert(curr_frame, [window_size - 1], predicted[-1], axis=0)
-#         prediction_seqs.append(predicted)
-#     return prediction_seqs
-
-
 def plot_results(predicted_data, true_data):
     fig = plt.figure(facecolor='white')
     ax = fig.add_subplot(111)
@@ -81,18 +56,5 @@ def plot_results(predicted_data, true_data):
     plt.plot(predicted_data, label='Prediction')
     plt.legend()
     plt.show()
-
-#
-# def plot_results_multiple(predicted_data, true_data, prediction_len):
-#     fig = plt.figure(facecolor='white')
-#     ax = fig.add_subplot(111)
-#     ax.plot(true_data, label='True Data')
-#     # Pad the list of predictions to shift it in the graph to it's correct start
-#     for i, data in enumerate(predicted_data):
-#         padding = [None for p in range(i * prediction_len)]
-#         plt.plot(padding + data, label='Prediction')
-#         plt.legend()
-#     plt.show()
-
 
 print('Support functions defined')
--- ThLSTM.py
+++ ThLSTM.py
@@ -18,15 +18,32 @@ print ('enrol window set to',Enrol_window )
 
 print('Support functions defined')
 
-dataset = pd.read_csv('./PMChineFiveCitie/ShenyangECXELPM20100101_20151231---V2).csv',sep=";", index_col='date', parse_dates=['date'])
+dataset = pd.read_csv('./PMChineFiveCitie/ShenyangECXELPM20100101_20151231---V2).csv',sep=";")
+# dataset = pd.read_csv('./PMChineFiveCitie/ShenyangECXELPM20100101_20151231---V2).csv',sep=";", index_col='date', parse_dates=['date'])
 # sep=";",
 # print('corrrr:',dataset.corr(method ='pearson'))
+
+
 print(dataset.head())
 
-feature_train, label_train, feature_test, label_test = load_data(dataset, 'PM_Taiyuanjie', Enrol_window, True)
 
-dataset["PM_Taiyuanjie"][:'2015'].plot(figsize=(16,4),legend=True)
-dataset["PM_Taiyuanjie"]['2015':].plot(figsize=(16,4),legend=True) # 10% is used for thraining data which is approx 2017 data
+
+dat=pd.to_datetime(dataset["date"])
+date =np.array(dat)
+
+one=np.array(dataset["PM_Taiyuanjie"])
+two= np.array(dataset["PM_US Post"])
+three = np.array(dataset["PM_Xiaoheyan"])
+# PM_Shenyang = np.column_stack((date,(one+two+three)/3))
+PM_Shenyang = np.array((one+two+three)/3)
+newPMdf = pd.DataFrame(data = {'PM_Shenyang':PM_Shenyang},index = date)
+print(newPMdf)
+
+# feature_train, label_train, feature_test, label_test = load_data(dataset, 'PM_Taiyuanjie', Enrol_window, True)
+feature_train, label_train, feature_test, label_test = load_data(newPMdf, 'PM_Shenyang', Enrol_window, True)
+
+newPMdf["PM_Shenyang"][:'2015'].plot(figsize=(16,4),legend=True)
+newPMdf["PM_Shenyang"]['2015':].plot(figsize=(16,4),legend=True) # 10% is used for thraining data which is approx 2017 data
 plt.legend(['Training set','Test set'])
 plt.title('True data')
 plt.show()
@@ -49,8 +66,3 @@ model.fit(feature_train, label_train, batch_size=512, epochs=5, validation_data 
 #Let's use the model and predict the stock
 predicted = model.predict(feature_test)
 plot_results(predicted,label_test)
-#
-
-
-
-
