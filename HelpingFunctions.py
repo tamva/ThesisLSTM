9cabd1dc440fbcf006e363ba6c9848b192925dbc
Klainti Isoufai <ki@progekta.gr>
2019-04-06 15:28:34 +0300

    r^2

--- HelpingFunctions.py
+++ HelpingFunctions.py
@@ -3,41 +3,55 @@ from numpy import newaxis
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM, GRU
+
 from keras.models import Sequential
+from sklearn.model_selection import train_test_split
 from keras import optimizers
+
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
 sc = MinMaxScaler(feature_range=(0, 1))
+# sc = MinMaxScaler()
 
-
-def load_data(datasetname, column, seq_len, normalise_window):
+def load_data(datasetname, column, seq_len, normalise_window): #it BRINGS: 1: The data, 2: Column Name to work with, 3: Just a number of Window (100), 4:Boolean value
     # A support function to help prepare datasets for an RNN/LSTM/GRU
-    data = datasetname.loc[:, column].replace(0, 1)#opou o vale 1
-    sequence_length = seq_len + 1
-    result = []
-    for index in range(len(data) - sequence_length): #krata ta data tou column xoris to window
-        result.append(data[index: index + sequence_length])
+    data = datasetname.loc[:, column].replace(0, 1)#Take the dataframe and get all the data from the column we want to work with and make zeros ones
+    sequence_length = seq_len + 1 # make  length value greater than 1
 
+    result = []                                 #https://machinelearningmastery.com/sequence-prediction/
+    for index in range(len(data) - sequence_length): #Pare thn  krata ta data tou column xoris to window
+        result.append(data[index: index + sequence_length])#https://datascience.stackexchange.com/questions/26366/training-an-rnn-with-examples-of-different-lengths-in-keras
+    
     if normalise_window:
         result = normalise_windows(result)
         result = sc.fit_transform(result)
 
     result = np.array(result)
+    X = result
+    train_size = int(len(X) * 0.66)
+    train, test= X[0:train_size], X[train_size:len(X)]
 
-    # Last 10% is used for validation test, first 90% for training
-    row = round(0.9 * result.shape[0])
-    train = result[:int(row), :]
-    np.random.shuffle(train)
+    #testsplit = int(len(test)* 0.55)
+    #tests, val = test[0:testsplit] , test[testsplit:len(test)]
+     
+    print('Data: %d' % (len(X)))
+    print('Training Dataset: %d' % (len(train)))
+    print('Testing Dataset: %d' % (len(test)))
+    #print('Test Validation: %d' % (len(val)))  
+    
+
     x_train = train[:, :-1]
     y_train = train[:, -1]
-    x_test = result[int(row):, :-1]
-    y_test = result[int(row):, -1]
+    x_test = test[:, :-1]
+    y_test = test[:, -1]
+     
 
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-    return [x_train, y_train, x_test, y_test]
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1)) #https://www.hackerrank.com/challenges/np-shape-reshape/problem
+    print(x_train.shape)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1)) #https://machinelearningmastery.com/index-slice-reshape-numpy-arrays-machine-learning-python/
+    
+    return [x_train, y_train, x_test, y_test, train_size, X]
 
 
 def normalise_windows(window_data):
@@ -50,11 +64,34 @@ def normalise_windows(window_data):
 
 
 def plot_results(predicted_data, true_data):
-    fig = plt.figure(facecolor='white')
+    # Standard plot /////////////////////////////////
+    fig = plt.figure(facecolor='white',figsize=(16,4))
     ax = fig.add_subplot(111)
     ax.plot(true_data, label='True Data')
-    plt.plot(predicted_data, label='Prediction')
-    plt.legend()
+    plt.plot(predicted_data,label='Prediction')
+    plt.legend(['True Data', 'Prediction'])
     plt.show()
 
+    # #SECONDARY PLOT /////////////////////////
+    # fig = plt.figure(facecolor='white',figsize=(16,4))
+    # ax = fig.add_subplot(111)
+    # ax.plot(true_data, label='True Data')
+    # plt.plot(predicted_data, label='Prediction')
+    # locs,labels = plt.xticks()
+    # labels = ['2012-06','2013-01','2013-06','2014-01','2014-06','2015-01','2015-12']
+    # plt.xticks(locs, labels)
+    # plt.show()
+
+
+# def plot_results_multiple(predicted_data, true_data):
+#     fig = plt.figure(facecolor='white')
+#     ax = fig.add_subplot(111)
+#     ax.plot(true_data, label='True Data')
+#     #Pad the list of predictions to shift it in the graph to it's correct start
+#     # for i, data in enumerate(predicted_data):
+#     data=10
+#     padding = predicted_data[-1]
+#     plt.plot(padding + data, label='Prediction')
+#     plt.legend()
+#     plt.show()
 print('Support functions defined')
--- ThLSTM.py
+++ ThLSTM.py
@@ -1,4 +1,6 @@
+import sys
 import numpy as np # linear algebra
+from keras.layers import BatchNormalization
 from numpy import newaxis
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from keras.layers.core import Dense, Activation, Dropout
@@ -8,8 +10,9 @@ from keras import optimizers
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from HelpingFunctions import *
-plt.style.use('fivethirtyeight')
+from sklearn.metrics import r2_score
 
+plt.style.use('fivethirtyeight')
 print ('import completed')
 
 Enrol_window = 100
@@ -18,7 +21,7 @@ print ('enrol window set to',Enrol_window )
 
 print('Support functions defined')
 
-dataset = pd.read_csv('./PMChineFiveCitie/ShenyangECXELPM20100101_20151231---V2).csv',sep=";")
+dataset = pd.read_csv('./Data/ShenyangECXELPM20100101_20151231---V2).csv',sep=";")
 # dataset = pd.read_csv('./PMChineFiveCitie/ShenyangECXELPM20100101_20151231---V2).csv',sep=";", index_col='date', parse_dates=['date'])
 # sep=";",
 # print('corrrr:',dataset.corr(method ='pearson'))
@@ -32,37 +35,74 @@ dat=pd.to_datetime(dataset["date"])
 date =np.array(dat)
 
 one=np.array(dataset["PM_Taiyuanjie"])
+one1 = one.tolist()
 two= np.array(dataset["PM_US Post"])
+two1 = two.tolist()
 three = np.array(dataset["PM_Xiaoheyan"])
-# PM_Shenyang = np.column_stack((date,(one+two+three)/3))
-PM_Shenyang = np.array((one+two+three)/3)
-newPMdf = pd.DataFrame(data = {'PM_Shenyang':PM_Shenyang},index = date)
+three1 = three.tolist()
+# one[(one == 0)&(two != 0) & (three != 0)] = (two + three)/2
+# PM_Shenyang = np.array((one+two+three)/3)
+PM_Shenyang = []
+for i in range(len(one1)):
+    if (one1[i] == 0 and two1[i] != 0 and three1[i] != 0) or (one1[i] != 0 and two1[i] == 0 and three1[i] != 0) or (
+            one1[i] != 0 and two1[i] != 0 and three1[i] == 0):
+
+        PM_Shenyang.append((one1[i] + two1[i] + three1[i])/2)
+
+    elif (one1[i] != 0 and two1[i] != 0 and three1[i] != 0):
+        PM_Shenyang.append((one1[i] + two1[i] + three1[i]) / 3)
+
+    elif (one1[i] == 0 and two1[i] == 0 and three1[i] != 0) or (one1[i] == 0 and two1[i] != 0 and three1[i] == 0) or (
+            one1[i] != 0 and two1[i] == 0 and three1[i] == 0):
+        PM_Shenyang.append(one1[i] + two1[i] + three1[i])
+
+    elif (one1[i] == 0 and two1[i] == 0 and three1[i] == 0) or (one1[i] == 0 and two1[i] == 0 and three1[i] == 0) or (
+            one1[i] == 0 and two1[i] == 0 and three1[i] == 0):
+        PM_Shenyang.append(0)
+
+PM_Shenyang1 = np.array(PM_Shenyang)
+
+#
+PM_Shenyang1[PM_Shenyang1 > 750] = 400
+print("PMnumbers", PM_Shenyang)
+newPMdf = pd.DataFrame(data = {'PM_Shenyang':PM_Shenyang1},index = date)
 print(newPMdf)
 
 # feature_train, label_train, feature_test, label_test = load_data(dataset, 'PM_Taiyuanjie', Enrol_window, True)
-feature_train, label_train, feature_test, label_test = load_data(newPMdf, 'PM_Shenyang', Enrol_window, True)
-
-newPMdf["PM_Shenyang"][:'2015'].plot(figsize=(16,4),legend=True)
-newPMdf["PM_Shenyang"]['2015':].plot(figsize=(16,4),legend=True) # 10% is used for thraining data which is approx 2017 data
-plt.legend(['Training set','Test set'])
+feature_train, label_train, feature_test, label_test, train_size, X = load_data(newPMdf, 'PM_Shenyang', Enrol_window, True)
+newPMdf["PM_Shenyang"][0:train_size].plot(figsize=(16,4),legend=True)
+newPMdf["PM_Shenyang"][train_size:len(X)].plot(figsize=(16,4),legend=True) # 10% is used for thraining data which is approx 2017 data
+plt.legend(['Training dataset','Testing dataset'])
 plt.title('True data')
 plt.show()
 
-
 model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(feature_train.shape[1],1)))
+model.add(LSTM(50, kernel_initializer='normal', return_sequences=True, input_shape=(feature_train.shape[1],1)))
 model.add(Dropout(0.2))
-model.add(LSTM(100, return_sequences=False))
+model.add(LSTM(100, kernel_initializer='normal', return_sequences=True))
 model.add(Dropout(0.2))
-model.add(Dense(1, activation = "linear"))
+model.add(LSTM(200, kernel_initializer='normal', return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(1,kernel_initializer='normal', activation='linear'))
 
-model.compile(loss='mse', optimizer='adam')
+model.compile(optimizer='adam', loss='mean_absolute_error', metrics=['mean_absolute_error'])
+# model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
-print ('model compiled')
+print('model compiled')
 
-model.fit(feature_train, label_train, batch_size=512, epochs=5, validation_data = (feature_test, label_test))
+model.fit(feature_train, label_train, batch_size=300, epochs=5, validation_split = 0.2)
 
 
-
+#Edw kanoume to prediction 
 predicted = model.predict(feature_test)
-plot_results(predicted,label_test,date)
+plot_results(predicted,label_test)
+
+#Edw kanoume to evaluation test
+kappa = model.evaluate(feature_test,label_test)
+#print('Accuracy: %.3f%%' % (kappa[1]*100))
+print('Evaluation Test: ', kappa)
+
+
+feature_train_pred = model.predict(feature_train)
+print("The R2 score on the Train set is:\t{:0.3f}".format(r2_score(label_train, feature_train_pred)))
+print("The R2 score on the Test set is:\t{:0.3f}".format(r2_score(label_test, predicted)))
