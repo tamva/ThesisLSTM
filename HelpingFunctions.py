b1c891e30c8b6708c2256f8c58c1cbd152ee5b73
Klainti Isoufai <ki@progekta.gr>
2019-04-16 23:53:21 +0300

    Add comment

---
+++ HelpingFunctions.py
@@ -0,0 +1,100 @@
+import numpy as np # linear algebra
+from numpy import newaxis
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM, GRU
+
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+from keras import optimizers
+
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+sc = MinMaxScaler(feature_range=(0, 1))
+
+
+
+#it BRINGS: 1: The data, 2: Column Name to work with, 3: Just a number of Window (100), 4:Boolean value
+def data_loader(datasetname, column, seq_len, normalise_window): 
+    #A support function for preparing data sets for an LSTM network
+    #Take the dataframe and get all the data from the column we want to work with and replace them from 0 to 1 in order to make the division 
+    data = datasetname.loc[:, column].replace(0, 1)
+    sequence_length = seq_len + 1 # make  length value greater than 1
+
+    result = []                               
+    for index in range(len(data) - sequence_length): #Pare thn  krata ta data tou column xoris to window
+        result.append(data[index: index + sequence_length])#https://datascience.stackexchange.com/questions/26366/training-an-rnn-with-examples-of-different-lengths-in-keras
+    
+    if normalise_window:
+        result = normalise_windows(result)
+        result = sc.fit_transform(result)
+
+    result = np.array(result)
+    X = result
+    train_size = int(len(X) * 0.66)
+    train, test= X[0:train_size], X[train_size:len(X)]
+
+    #testsplit = int(len(test)* 0.55)
+    #tests, val = test[0:testsplit] , test[testsplit:len(test)]
+     
+    print('Data: %d' % (len(X)))
+    print('Training Dataset: %d' % (len(train)))
+    print('Testing Dataset: %d' % (len(test)))
+    #print('Test Validation: %d' % (len(val)))  
+    
+
+    x_train = train[:, :-1]
+    y_train = train[:, -1]
+    x_test = test[:, :-1]
+    y_test = test[:, -1]
+     
+
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1)) #https://www.hackerrank.com/challenges/np-shape-reshape/problem
+    print(x_train.shape)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1)) #https://machinelearningmastery.com/index-slice-reshape-numpy-arrays-machine-learning-python/
+    
+    return [x_train, y_train, x_test, y_test, train_size, X]
+
+
+def normalise_windows(window_data):
+    # Prepare the dataset, note that the PM  price data will be normalized between 0 and 1
+    normalised_data = []
+    for window in window_data:
+        normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
+        normalised_data.append(normalised_window)
+    return normalised_data
+
+
+def plot_results(predicted_data, true_data):
+    # Standard plot /////////////////////////////////
+    fig = plt.figure(facecolor='white',figsize=(16,4))
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data,label='Prediction')
+    plt.legend(['True Data', 'Prediction'])
+    plt.show()
+
+    # #SECONDARY PLOT /////////////////////////
+    # fig = plt.figure(facecolor='white',figsize=(16,4))
+    # ax = fig.add_subplot(111)
+    # ax.plot(true_data, label='True Data')
+    # plt.plot(predicted_data, label='Prediction')
+    # locs,labels = plt.xticks()
+    # labels = ['2012-06','2013-01','2013-06','2014-01','2014-06','2015-01','2015-12']
+    # plt.xticks(locs, labels)
+    # plt.show()
+
+
+# def plot_results_multiple(predicted_data, true_data):
+#     fig = plt.figure(facecolor='white')
+#     ax = fig.add_subplot(111)
+#     ax.plot(true_data, label='True Data')
+#     #Pad the list of predictions to shift it in the graph to it's correct start
+#     # for i, data in enumerate(predicted_data):
+#     data=10
+#     padding = predicted_data[-1]
+#     plt.plot(padding + data, label='Prediction')
+#     plt.legend()
+#     plt.show()
+print('Support functions defined')
---
+++ ThLSTM.py
@@ -0,0 +1,138 @@
+import sys
+import numpy as np # linear algebra
+from keras.layers import BatchNormalization
+from numpy import newaxis
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM, GRU
+from keras.models import Sequential
+from keras import optimizers
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from HelpingFunctions import * #we import our Helping Function file that contains all the functions
+from sklearn.metrics import r2_score
+
+
+
+#Here is the style that we use in our plots
+plt.style.use('fivethirtyeight')
+print ('import completed')
+
+
+# Enter the steps to register on the network.
+#RNN / LSTM / GRU can be taught at times times as large as the number of times you register them, and not longer (a fundamental limitation).
+# So by design these networks are deep / long to catch repeating motifs.
+Enrol_window = 100
+
+print ('enrol window set to',Enrol_window )
+
+print('Support functions defined')
+
+
+#Load the data
+dataset = pd.read_csv('./Data/ShenyangECXELPM20100101_20151231---V2).csv',sep=";")
+
+
+print(dataset.head())
+
+
+#Creat the list which
+dat=pd.to_datetime(dataset["date"])
+date =np.array(dat)
+
+
+#Here we create three arrays with all PM station measurement of Shenyang and covert them to lists so then we can edit them one by one
+one=np.array(dataset["PM_Taiyuanjie"])
+one1 = one.tolist()
+two= np.array(dataset["PM_US Post"])
+two1 = two.tolist()
+three = np.array(dataset["PM_Xiaoheyan"])
+three1 = three.tolist()
+
+
+
+#In order to calculate the average value of Shenyang we take the inputs of each station that have non zero values 
+#and divide them with the sum of the station that has non zero values 
+PM_Shenyang = []
+for i in range(len(one1)):
+    #Here we calculate the average if one station has 0 measurment 
+    if (one1[i] == 0 and two1[i] != 0 and three1[i] != 0) or (one1[i] != 0 and two1[i] == 0 and three1[i] != 0) or (
+            one1[i] != 0 and two1[i] != 0 and three1[i] == 0):
+
+        PM_Shenyang.append((one1[i] + two1[i] + three1[i])/2)
+
+    #Here we calculate the average if there is no zero values in station
+    elif (one1[i] != 0 and two1[i] != 0 and three1[i] != 0):
+        PM_Shenyang.append((one1[i] + two1[i] + three1[i]) / 3)
+
+    #Here we calculate the average if 2 of the station has zero values
+    elif (one1[i] == 0 and two1[i] == 0 and three1[i] != 0) or (one1[i] == 0 and two1[i] != 0 and three1[i] == 0) or (
+            one1[i] != 0 and two1[i] == 0 and three1[i] == 0):
+        PM_Shenyang.append(one1[i] + two1[i] + three1[i])
+
+    #Here we calulate the average if 3 of the station has zero values
+    elif (one1[i] == 0 and two1[i] == 0 and three1[i] == 0) or (one1[i] == 0 and two1[i] == 0 and three1[i] == 0) or (
+            one1[i] == 0 and two1[i] == 0 and three1[i] == 0):
+        PM_Shenyang.append(0)
+
+PM_Shenyang1 = np.array(PM_Shenyang)
+
+#In order to overtake the error values we replace all the values over 750 with the number 400
+PM_Shenyang1[PM_Shenyang1 > 750] = 400
+print("PMnumbers ", PM_Shenyang)
+
+#This is the finale dataframe that contains all the values for the prediction 
+newPMdf = pd.DataFrame(data = {'PM_Shenyang':PM_Shenyang1},index = date)
+print(newPMdf)
+
+
+# Prepare the dataset, note that the PM 2.5 values for PM_Shenyang data will be normalized between 0 and 1
+# A feature is an input variable, in this case a PM 2.5 Value
+# Selected 'PM_Shenyang' PM 2.5 Values
+feature_train, label_train, feature_test, label_test, train_size, X = data_loader(newPMdf, 'PM_Shenyang', Enrol_window, True)
+newPMdf["PM_Shenyang"][0:train_size].plot(figsize=(16,4),legend=True)
+newPMdf["PM_Shenyang"][train_size:len(X)].plot(figsize=(16,4),legend=True) # 10% is used for thraining data which is approx 2017 data
+plt.legend(['Training dataset','Testing dataset'])
+plt.title('True data')
+plt.show()
+
+
+
+#The LSTM model we will test, lets see if the sinus prediction results can be matched
+
+#Here we start the model building structure 
+model = Sequential()
+model.add(LSTM(50, kernel_initializer='normal', return_sequences=True, input_shape=(feature_train.shape[1],1)))
+model.add(Dropout(0.2))
+model.add(LSTM(100, kernel_initializer='normal', return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(200, kernel_initializer='normal', return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(1,kernel_initializer='normal', activation='linear'))
+
+model.compile(optimizer='adam', loss='mean_absolute_error', metrics=['mean_absolute_error'])
+# model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+print('model compiled')
+
+
+#Train the model with Keras fit function
+model.fit(feature_train, label_train, batch_size=300, epochs=5, validation_split = 0.2)
+
+
+#Here we use the model and predict the PM2.5 of Shenyang
+predicted = model.predict(feature_test)
+plot_results(predicted,label_test)
+
+
+
+
+#Here we do the evaluation test
+kappa = model.evaluate(feature_test,label_test)
+
+print('Evaluation Test: ', kappa)
+
+#Here we calculate the R^2 score of training kai testing set on our model
+feature_train_pred = model.predict(feature_train)
+print("The R2 score on the Train set is:\t{:0.3f}".format(r2_score(label_train, feature_train_pred)))
+print("The R2 score on the Test set is:\t{:0.3f}".format(r2_score(label_test, predicted)))
